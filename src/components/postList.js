7640b2020522ecc2af3ab1d5c2828f07ba281370
Jia <ilovemit2013@gmail.com>
2020-03-31 16:04:40 +0800

    2

--- src/components/postList.js
+++ src/components/postList.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, FlatList} from 'react-native';
+
+const postList =() =>{
+    return(
+        <View>
+
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default PostList;
